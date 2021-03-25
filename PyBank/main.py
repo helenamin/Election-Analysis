5299779193ffb46394c6aaca73ec65c62f35b5cf
Helen Amin <helen.amin@ltt.edu.au>
2021-03-25 11:20:43 +0800

    Update Part One -PYBank

---
+++ PyBank/main.py
@@ -0,0 +1,78 @@
+#Module to create file paths across operating systems
+import os
+# Module for reading CSV files
+import csv
+
+import statistics
+
+import string
+
+# Path to collect data from the Resources folder
+dirname = os.path.dirname(__file__)
+bank_csv = os.path.join(dirname, "Resources", "budget_data.csv")
+
+#Path to write the result
+output_path = os.path.join(dirname,"analysis","result.txt")
+
+totalMonths =0
+totalProfitLoss =0
+previousProfitLoss =0
+changes={}
+averageChange =0
+gip =""
+gdp =""
+message ="\n"
+message_list =[]
+
+#Reading using CSV module
+with open(bank_csv,'r') as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile,delimiter =",")
+
+    # Read the header row first (skip this step if there is now header)
+    csv_header = next(csvreader)
+    
+    # Read each row of data after the header
+    for row in csvreader:
+
+        #Calculates total number of months
+        totalMonths +=1   
+        #Calculates the net total amount of "Profit/Losses"
+        totalProfitLoss +=int(row[1])   
+        
+        # Creates changes dictionary with date as index and change as value
+        changes[row[0]] = int(row[1])-previousProfitLoss
+        #Changes previousProfitLoss variable to current ProfitLoss, preparing the variable for next iteration moving down the list
+        previousProfitLoss =int(row[1])
+    
+    
+
+    #Calculates the average change
+    averageChange = statistics.mean(changes.values()) 
+    
+    #finds the greatest increase in profits and greatest decrease in losses (date and amount for both)
+    for date, change in changes.items():
+        if change == max(changes.values()) :
+            gip = "{} ({})".format(date,change)
+        if change == min(changes.values()):
+            gdp = "{} ({})".format(date,change)
+
+
+#Create Financial Analysis result
+message_list= ["\n","Financial Analysis","----------------------------",\
+    f"Total Months : {str(totalMonths)}",f"Total : {str(totalProfitLoss)}",\
+    f"Average Change : {averageChange}",f"Greatest Increase in Profits : {gip}",\
+    f"Greatest Increase in Profits : {gdp}","----------------------------"]
+
+message = message.join(message_list)
+
+#print Financial Analysis result into terminal
+print(message)
+
+#Write the result into result.txt file
+outF = open(output_path,'w')
+# Write result using writelines()
+outF.writelines(message)
+outF.close()
+    
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # python-challenge
+This contains two challenges: PyBank and  PyPoll.
+
+PyBank
+    Provided python script alanyzes the records to calculate:
+        The total number of months included in the dataset
+        The net total amount of "Profit/Losses" over the entire period
+        The average of the changes in "Profit/Losses" over the entire period
+        The greatest increase in profits (date and amount) over the entire period
+        The greatest decrease in losses (date and amount) over the entire period
+    The script prints the analysis to the terminal as well as exporting the result to a text file .
